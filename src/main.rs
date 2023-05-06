8b2602be6fc34678332a5a13d026329399bda208
Tobias Frei <shuntingyard@gmail.com>
2023-05-06 20:53:21 +0200

    pagination through results done, still no persistence nor error matching

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,63 @@ struct Oauth1Fields {
     secret: String,
 }
 
-async fn task_that_takes_a_second(auth: Oauth1aToken) -> Result<(), Box<dyn std::error::Error>> {
-    // Try lookin' up followers in user's context.
-    let my_followers = TwitterApi::new(auth)
+async fn do_work_interval(
+    auth: Oauth1aToken,
+    pagination_token: &Option<String>,
+) -> Result<Option<String>, Box<dyn std::error::Error>> {
+    // We can't get away [400 Bad Request] with null length &str as next_token so:
+
+    // 1) let's prepare common query parms.
+    let field_array = [UserField::Id, UserField::Username];
+    let max = 30;
+
+    // 2)   code separate queries, depending on presence of pagination token
+    //      for lookin' up followers in user's context.
+    let api_response;
+
+    if let Some(next_token) = pagination_token {
+        api_response = TwitterApi::new(auth)
         .with_user_ctx()
         .await?
         //.expect("Schnauzfall bim konteggscht")
         .get_my_followers()
-        .user_fields([UserField::Id, UserField::Username])
+        .user_fields(field_array)
+        .max_results(max)
+        .pagination_token(next_token)
         .send()
         .await?
         //.expect("Schnauzfall bi dä follis")
-        .into_data();
+        ;
+    } else {
+        api_response = TwitterApi::new(auth)
+        .with_user_ctx()
+        .await?
+        //.expect("Schnauzfall bim konteggscht")
+        .get_my_followers()
+        .user_fields(field_array)
+        .max_results(max)
+        .send()
+        .await?
+        //.expect("Schnauzfall bi dä follis")
+        ;
+    }
+
+    // Destructure metadata and get next_token.
+    let next_token: Option<String>;
+    if let Some(meta) = &api_response.meta {
+        next_token = match &meta.next_token {
+            Some(t) => Some(String::from(t)),
+            _ => None,
+        };
+        println!(
+            "result_count: {}, previous_token: {:?}",
+            meta.result_count, &meta.previous_token
+        );
+    } else {
+        next_token = None;
+    }
+
+    let my_followers = api_response.into_data();
 
     // 0 follies will be `None` and not enter here.
     if let Some(my_followers) = my_followers {
@@ -36,16 +81,15 @@ async fn task_that_takes_a_second(auth: Oauth1aToken) -> Result<(), Box<dyn std:
         println!(" --total: {subtot}\n");
     };
 
-    Ok(())
+    Ok(next_token)
 }
 
 #[tokio::main]
 async fn main() {
     // Get authorization:
-    let creds = serde_json::from_str::<Oauth1Fields>(include_str!(
-        "../../ferristw2/Oauth1UsrCtxRight.json"
-    ))
-    .expect("Oauth1 token d3s3r1al1Ze tr0ubl3");
+    let creds =
+        serde_json::from_str::<Oauth1Fields>(include_str!("../../ferristw2/Oauth1UsrCtxLeft.json"))
+            .expect("Oauth1 token d3s3r1al1Ze tr0ubl3");
 
     let auth = Oauth1aToken::new(
         creds.consumer_key,
@@ -56,9 +100,14 @@ async fn main() {
 
     // Start doing the work.
     let mut interval = time::interval(time::Duration::from_secs(30));
+    let mut next: Option<String> = None; // next_token
+
     loop {
         interval.tick().await;
-        if let Err(e) = task_that_takes_a_second(auth.clone()).await {
+        let result = do_work_interval(auth.clone(), &next).await;
+        //
+        // All errors treated here...
+        if let Err(e) = result {
             /*
                 TODO: so far (stupidly) we don't know how to match here!
 
@@ -102,6 +151,13 @@ async fn main() {
                 Possible solution using the `thiserror` crate with '?-operator' and `tokio`?
             */
             eprintln!("ERROR: {e}");
+        } else {
+            //
+            // Manage next_token, be it empty or not.
+            next = match result {
+                Ok(t) => t,
+                Err(_) => None,
+            };
         }
     }
 }
